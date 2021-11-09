db8b845dd0b72ded103177608b19c76d7c60f8b4
Niyarkalita2021 <82107686+Niyarkalita2021@users.noreply.github.com>
2021-11-09 20:56:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,4 +25,30 @@ video.loop();
 video.speed(1);
 video.volume(0);
 }
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+      console.log(results);
+      objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of ojects detected are : "+ objects.length;
+
+            fill("#FFFF00");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FFFF00");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
     
